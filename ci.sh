3354343368120b3fc01b9f749192e7523fb51ff7
YouMFua <dimon2005hrw@gmail.com>
2025-10-10 01:06:23 +0300

    fixed more problems

--- ci.sh
+++ ci.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e  # зупинятись при помилках
 
+cd /tmp
+cp -r /mnt/c/csad2526KI409HarvatDmytro05 ./project
+cd project
+
 echo "=== Cleaning previous build ==="
 rm -rf build
 
@@ -8,11 +12,16 @@ echo "=== Creating build directory ==="
 mkdir -p build
 cd build
 
-echo "=== Configuring project with MinGW / GCC ==="
-cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
+echo "=== Detecting platform and configuring ==="
+case "$(uname -s)" in
+  Linux*)   cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release .. ;;
+  Darwin*)  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release .. ;;
+  MINGW*|MSYS*|CYGWIN*) cmake -G "Ninja" -A x64 -DCMAKE_BUILD_TYPE=Release .. ;;
+  *) echo "Unknown OS"; exit 1 ;;
+esac
 
 echo "=== Building project ==="
-mingw32-make
+cmake --build . --config Release
 
 echo "=== Running tests ==="
 ctest --output-on-failure
